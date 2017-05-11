a66da0bb96ce22dc90401244da2311db0f6b3b00
李冠军 <atomgj@gmail.com>
2017-05-11 11:36:43 +0800

    mysql

--- mysql/app.js
+++ mysql/app.js
@@ -2,26 +2,28 @@ var http = require('http'),
     mysql = require('mysql'),
     db,
     server
-    ;
+;
 
 db = mysql.createConnection({
     host : "127.0.0.1",
-    user: "root",
+    user : "root",
     password : "root",
     database : "hello-node"
 });
 
+
 server = http.createServer(function(request, response){
 
-    response.writeHead(200, {
-        'Content-Type' : 'text/paint',
-        'Access-Control-Allow-Origin': '*'
-    });
 
-    db.query('select 1 as count;', function(error, rows, fields){
-        response.end(JSON.stringify(rows));
-    });
+     response.writeHead(200, {
+          "Content-Type" : "text/plain"
+     });
+     db.query("select 1 as cost;", function(error, rows, fields){
+ 	  response.end(JSON.stringify(rows));
+     });
 
 });
+
+
 
 server.listen(3000);
