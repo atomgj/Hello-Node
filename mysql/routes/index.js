f1e709e2fecf7db84878c6d5f42c1e43830c0ad9
李冠军 <atomgj@gmail.com>
2017-05-26 11:32:08 +0800

    add routes

---
+++ mysql/routes/index.js
@@ -0,0 +1,12 @@
+module.exports = function(obj){
+   var urlMap = {
+       '/cart' : getCart,
+       '/commodity' : getComodity,
+       '/list' : getList
+   };  
+
+   var getCart = function(){
+
+
+   };
+};
